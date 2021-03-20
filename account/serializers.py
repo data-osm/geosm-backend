1523b13583b18ee022ddc7d16b881f8681b17f4c
KARL Tayou <nelsontayou1@gmail.com>
2021-03-20 17:45:22 +0100

    add user as super user

--- account/serializers.py
+++ account/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import User
+from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 
 class userSerializer(serializers.ModelSerializer):
     # user=serializers.StringRelatedField(read_only=True)
     class Meta:
         model=User
-        fields=['created_at','email','id','is_active','is_superuser','updated_at','username']
+        fields=['created_at','email','id','is_active','is_superuser','updated_at','username','last_name']
+
+class UserRegistrationSerializer(BaseUserRegistrationSerializer):
+    class Meta(BaseUserRegistrationSerializer.Meta):
+        fields = ('id', 'email', 'last_name', 'is_superuser', 'password','username' )
--- settings/base.py
+++ settings/base.py
@@ -67,6 +67,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+DJOSER = {
+    'SERIALIZERS': {
+         'user_create': 'account.serializers.UserRegistrationSerializer'
+    }
+}
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:9000"
