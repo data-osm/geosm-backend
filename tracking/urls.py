817a9151af61e90ec5c458e870ed97e57d481461
KARL Tayou <nelsontayou1@gmail.com>
2021-03-21 16:44:36 +0100

    minor update

--- tracking/urls.py
+++ tracking/urls.py
@@ -2,6 +2,7 @@ from .views import TrackingEventListView
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+# http://127.0.0.1:8000/api/tracking/list?object_id=2&model_name=metadata
 urlpatterns = [
     path("list",TrackingEventListView.as_view(),name="tracking"),
 ]
