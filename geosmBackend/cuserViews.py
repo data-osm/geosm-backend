1dadf558aa7d0a2872ffc85a9735e93ec09e05a0
KARL Tayou <nelsontayou1@gmail.com>
2022-01-02 20:05:00 +0100

    document views of OSM group

--- geosmBackend/cuserViews.py
+++ geosmBackend/cuserViews.py
@@ -2,6 +2,17 @@ from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
 from cuser.middleware import CuserMiddleware
 
+class EnablePartialUpdateMixin:
+    """Enable partial updates
+    https://tech.serhatteker.com/post/2020-09/enable-partial-update-drf/
+
+    Override partial kwargs in UpdateModelMixin class
+    https://github.com/encode/django-rest-framework/blob/91916a4db14cd6a06aca13fb9a46fc667f6c0682/rest_framework/mixins.py#L64
+    """
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
+        
 class RetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
                                    mixins.UpdateModelMixin,
                                    mixins.DestroyModelMixin,
--- osm/views.py
+++ osm/views.py
@@ -8,7 +8,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
-from geosmBackend.cuserViews import (ListCreateAPIView,RetrieveUpdateDestroyAPIView,)
+from geosmBackend.cuserViews import (EnablePartialUpdateMixin ,UpdateAPIView, RetrieveUpdateAPIView, CreateAPIView)
 from rest_framework.views import APIView
 from rest_framework import status
 from django.db.models import Count
@@ -16,18 +16,38 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 from django.core.exceptions import ObjectDoesNotExist
 from geosmBackend.type import httpResponse
 from cuser.middleware import CuserMiddleware
-
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import osmQuerrySerializer
 from collections import defaultdict
 # Create your views here.
+
+
+
+class CreateOsmQuerryView(CreateAPIView):
+   
+    queryset=Querry.objects.all()
+    serializer_class=osmQuerrySerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        responses={200: osmQuerrySerializer()},
+        tags=['OSM provider'],
+    )
+    def post(self, request, *args, **kwargs):
+        """ Create an osm provider  """
+        return super(CreateOsmQuerryView, self).post(request, *args, **kwargs)
 
 class osmQuerryView(APIView):
     """
         View to add an osm querry
     """
     permission_classes = [permissions.IsAuthenticated]
+    @swagger_auto_schema(
+        responses={200: osmQuerrySerializer()},
+        tags=['OSM provider'],
+    )
     def get(self, request, pk):
-        """ get an osm provider """
+        """ Retrieve an osm provider by his primary key """
         saved_querry = get_object_or_404(Querry.objects.all(), pk=pk)
         op_serializer = osmQuerrySerializer(instance=saved_querry, data=request.data, partial=True)
         if op_serializer.is_valid(raise_exception=True):
@@ -35,6 +55,11 @@ class osmQuerryView(APIView):
         else:
             return Response(httpResponse(error=True,msg=op_serializer.errors).toJson(), status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(
+        query_serializer=osmQuerrySerializer,
+        responses={200: osmQuerrySerializer()},
+        tags=['OSM provider'],
+    )
     def put(self, request, pk):
         """ update an osm querry """
         CuserMiddleware.set_user(request.user)
@@ -49,18 +74,3 @@ class osmQuerryView(APIView):
            
         else:
             return Response(op_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def post(self, request, *args, **kwargs):
-        """ store a new osm querry"""
-        CuserMiddleware.set_user(request.user)
-        op_serializer = osmQuerrySerializer(data=request.data)
-        if op_serializer.is_valid():
-            try:
-                op_serializer.save()
-                return Response(op_serializer.data, status=status.HTTP_201_CREATED)
-            except Exception as e:
-                return Response(httpResponse(error=True,msg=str(e)).toJson(), status=status.HTTP_400_BAD_REQUEST)
-
-        else:
-            return Response(op_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
